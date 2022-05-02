e791bc7e782a6e7747f4875856295f2b91d6ec9c
arpantubai <arpankng93@gmail.com>
2022-05-02 16:32:28 +0530

    [Arpan]:Added implemented programs using if,elif in Shell Scripting Programs

---
+++ practice_problem8/practice1.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -x
+
+random=$((1$RANDOM%6))
+
+declare -A dice
+for((i=0; i<6; i++))
+do
+        j=$(($i+1))
+        dice[$j]=0
+
+done
+
+while((1))
+do
+
+random=$((1+$RANDOM%6))
+
+dice[$random]=$((${dice[$random]}+1))
+
+if(( ${dice[$random]}==10 ))
+then
+break
+fi
+done
+
+echo "the key of dice :" ${!dice[@]}
+echo "the values of dice :" ${dice[@]}
+
+temp=7
+for i in ${!dice[@]}
+do
+        echo $i rolled  ${dice[$i]} times
+
+        if((${dice[$i]}<$temp))
+        then
+        temp=${dice[$i]}
+        j=$i
+        fi
+
+done
+
+echo "Minimum times rolled is : $j"
+
+temp=7
+for i in ${!dice[@]}
+do
+        echo $i rolled  ${dice[$i]} times
+
+        if((${dice[$i]}>$temp))
+        then
+        temp=${dice[$i]}
+        j=$i
+        fi
+
+done
+
+echo "Maximum times rolled is : $j"
